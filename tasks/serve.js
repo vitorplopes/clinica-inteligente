72ee9277721a0b9738093f1a193cde3ef312f674
Vitor Lopes <Vitor Lopes>
2016-03-08 02:07:36 -0300

    t

--- tasks/serve.js
+++ tasks/serve.js
@@ -8,12 +8,9 @@ var path = require('path');
 gulp.task('serve-dev', ['wiredep', 'tsc-app', 'watch-ts', 'watch-sass'], function () {
     //liveServer.start(config.liveServer.dev);
     connect.server({
-      root: path.resolve('./'),
+      root: '',
       port: process.env.PORT || 5000, // localhost:5000
-      livereload: false,
-      open: {
-        file: 'index.html'
-      }
+      livereload: false
     });
 });
 
