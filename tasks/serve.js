263ff73d8096d9fdeafeee6190ddb7d15a0b26db
Vitor Lopes <Vitor Lopes>
2016-03-08 01:28:12 -0300

    t

--- tasks/serve.js
+++ tasks/serve.js
@@ -3,11 +3,16 @@ var liveServer = require('live-server');
 var config = require('../gulp.config')();
 
 /* Start live server dev mode */
-gulp.task('serve-dev', ['wiredep', 'tsc-app', 'watch-ts', 'watch-sass'], function () {  
+gulp.task('serve-dev', ['wiredep', 'tsc-app', 'watch-ts', 'watch-sass'], function () {
     liveServer.start(config.liveServer.dev);
 });
 
 /* Start live server production mode */
 gulp.task('serve-build', ['build'], function () {
-    liveServer.start(config.liveServer.prod);
+  //  liveServer.start(config.liveServer.prod);
+  connect.server({
+    root: [__dirname],
+    port: process.env.PORT || 5000, // localhost:5000
+    livereload: false
+  });
 });
